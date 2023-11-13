5a7b903e331273f8b6488db3913c387200d21bf7
Maulana Imanulhaq <imanulhaq5@gmail.com>
2023-11-13 13:35:23 +0700

    api

--- assets/js/gis/template/template.js
+++ assets/js/gis/template/template.js
@@ -1,5 +1,5 @@
-export let URLGeoJson = "https://asia-southeast2-annular-hexagon-401501.cloudfunctions.net/BuatAmbilDataGeojsonToken";
-export let URLPostPoint = "https://asia-southeast2-annular-hexagon-401501.cloudfunctions.net/BuatGeojsonPolylineToken";
+export let URLGeoJson = "https://asia-southeast2-annular-hexagon-401501.cloudfunctions.net/BuatAmbilDataGeojson1";
+export let URLPostPoint = "https://asia-southeast2-annular-hexagon-401501.cloudfunctions.net/BuatGeojsonPolyline1";
 export let tableTag="tr";
 export let tableRowClass="content is-small";
 export let tableTemplate=`
